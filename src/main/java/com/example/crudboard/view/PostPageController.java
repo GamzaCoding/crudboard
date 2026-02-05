836306066ed972da9be04b6a18db06542c59938e
HyeongSeok <hyeongseok1995@gmail.com>
2026-02-05 18:36:27 +0900

    feat: 게시글 화면(list/detail/new/edit) 초기 구현, 공통 CSS 추가

---
+++ src/main/java/com/example/crudboard/view/PostPageController.java
@@ -0,0 +1,27 @@
+package com.example.crudboard.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/posts")
+public class PostPageController {
+
+    @GetMapping
+    public String listPage() {
+        return "posts/list";
+    }
+    @GetMapping("/new")
+    public String newPage() {
+        return "posts/new";
+    }
+    @GetMapping("/{id}")
+    public String detailPage() {
+        return "posts/detail";
+    }
+    @GetMapping("/{id}/edit")
+    public String editPage() {
+        return "posts/edit";
+    }
+}
