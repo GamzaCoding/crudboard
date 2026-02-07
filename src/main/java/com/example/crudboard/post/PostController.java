1fd380dc8c99d681154aee2fc052f3613b41fc8d
HyeongSeok <hyeongseok1995@gmail.com>
2026-02-07 14:28:48 +0900

    feat: detail.html에 댓글 목록/작성/삭제 UI 추가

--- src/main/java/com/example/crudboard/post/PostController.java
+++ src/main/java/com/example/crudboard/post/PostController.java
@@ -17,7 +17,6 @@ import jakarta.validation.Valid;
 import java.net.URI;
 import java.time.LocalDateTime;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
